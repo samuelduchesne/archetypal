c9cf21fd053a6bb76e3fdbd6b5009167c3d1a663
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2018-09-19 13:42:59 -0400

    Better settings

--- pyumi/__init__.py
+++ pyumi/__init__.py
@@ -2,4 +2,3 @@ from .load_umi_template import *
 from .simple_glazing import *
 from .idf import *
 from .utils import *
-from .settings import *
--- pyumi/settings.py
+++ pyumi/settings.py
@@ -1,6 +1,5 @@
 import logging as lg
 
-
 # locations to save data, logs, images, and cache
 data_folder = 'data'
 logs_folder = 'logs'
@@ -13,6 +12,12 @@ use_cache = False
 # write log to file and/or to console
 log_file = False
 log_console = False
+log_notebook = False
 log_level = lg.INFO
-log_name = 'pyumi-log'
+log_name = 'pyumi'
 log_filename = 'pyumi'
+
+# usual idfobjects
+useful_idf_objects = ['WINDOWMATERIAL:GAS', 'WINDOWMATERIAL:GLAZING', 'WINDOWMATERIAL:SIMPLEGLAZINGSYSTEM',
+                      'MATERIAL', 'MATERIAL:NOMASS', 'CONSTRUCTION', 'BUILDINGSURFACE:DETAILED',
+                      'FENESTRATIONSURFACE:DETAILED', 'SCHEDULE:DAY:INTERVAL', 'SCHEDULE:WEEK:DAILY', 'SCHEDULE:YEAR']

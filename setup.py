4968002c538eafcfaf640d36fa9d9bf6d228a89f
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2018-11-12 15:44:23 -0500

    Added package setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyumi',
+    version='1.0',
+    packages=['pyumi', 'pyumi.tests'],
+    url='',
+    license='MIT License',
+    author='Samuel Letellier-Duchesne',
+    author_email='samuel.letellier-duchesne@polymtl.ca',
+    description='',
+    install_requires=['numpy', 'pandas', 'eppy']
+)

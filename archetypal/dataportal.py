d0dbb40632ab46dba3470e8a6f8ea5d246161a7f
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2019-01-11 16:59:09 -0500

    New: opeinei, nrel, bcl, gis_server portals

--- archetypal/dataportal.py
+++ archetypal/dataportal.py
@@ -3,12 +3,22 @@ import io
 import json
 import logging as lg
 import os
+import re
+import time
 
 import pandas as pd
+import geopandas as gpd
+from sqlalchemy import create_engine
 import pycountry as pycountry
 import requests
 
 from archetypal import log, settings, make_str
+
+# scipy and sklearn are optional dependencies for faster nearest node search
+try:
+    from osgeo import gdal
+except ImportError as e:
+    gdal = None
 
 
 def tabula_available_buildings(code_country='France'):
@@ -360,7 +370,6 @@ def save_to_cache(url, response_json):
             cache_path_filename = os.path.join(settings.cache_folder,
                                                os.extsep.join(
                                                    [filename, 'json']))
-
             # dump to json, and save to file
             json_str = make_str(json.dumps(response_json))
             with io.open(cache_path_filename, 'w',
@@ -368,3 +377,322 @@ def save_to_cache(url, response_json):
                 cache_file.write(json_str)
 
             log('Saved response to cache file "{}"'.format(cache_path_filename))
+
+
+def openei_api_request(data, pause_duration=None, timeout=180,
+                       error_pause_duration=None):
+    """
+
+    Args:
+        data (dict or OrderedDict): key-value pairs of parameters to post to
+            the API
+        pause_duration:
+        timeout (int): how long to pause in seconds before requests, if None,
+            will query API status endpoint to find when next slot is available
+        error_pause_duration (int): the timeout interval for the requests
+            library
+
+    Returns:
+        dict
+    """
+    # define the Overpass API URL, then construct a GET-style URL as a string to
+    # hash to look up/save to cache
+    url = ' https://openei.org/services/api/content_assist/recommend'
+    prepared_url = requests.Request('GET', url, params=data).prepare().url
+    cached_response_json = get_from_cache(prepared_url)
+
+    if cached_response_json is not None:
+        # found this request in the cache, just return it instead of making a
+        # new HTTP call
+        return cached_response_json
+
+
+# def openei_dataset_request(data):
+#     'COMMERCIAL_LOAD_DATA_E_PLUS_OUTPUT'
+#     'https://openei.org/datasets/files/961/pub/{}
+#     'USA_AR_Batesville'
+#     'AWOS'
+#     '723448'
+#     'RefBldgMediumOfficeNew2004'
+#     '/{}.{}.{}_TMY3/{}_v1.3_7.1_3A_USA_GA_ATLANTA\
+#         .csv'
+
+
+def nrel_api_cbr_request(data):
+    # define the Overpass API URL, then construct a GET-style URL as a string to
+    # hash to look up/save to cache
+    url = 'https://developer.nrel.gov/api/commercial-building-resources/v1' \
+          '/resources.json'
+    prepared_url = requests.Request('GET', url, params=data).prepare().url
+    cached_response_json = get_from_cache(prepared_url)
+
+    if cached_response_json is not None:
+        # found this request in the cache, just return it instead of making a
+        # new HTTP call
+        return cached_response_json
+
+    else:
+        start_time = time.time()
+        log('Getting from {}, "{}"'.format(url, data))
+        response = requests.get(prepared_url)
+        # if this URL is not already in the cache, pause, then request it
+        # get the response size and the domain, log result
+        size_kb = len(response.content) / 1000.
+        domain = re.findall(r'//(?s)(.*?)/', url)[0]
+        log('Downloaded {:,.1f}KB from {}'
+            ' in {:,.2f} seconds'.format(size_kb, domain,
+                                         time.time() - start_time))
+
+        try:
+            response_json = response.json()
+            if 'remark' in response_json:
+                log('Server remark: "{}"'.format(response_json['remark'],
+                                                 level=lg.WARNING))
+            save_to_cache(prepared_url, response_json)
+        except Exception:
+            # deal with response satus_code here
+            log(
+                'Server at {} returned status code {} and no JSON data.'.format(
+                    domain,
+                    response.status_code),
+                level=lg.ERROR)
+        else:
+            return response_json
+
+
+def nrel_bcl_api_request(data):
+    """Send a request to the Building Component Library API via HTTP GET and
+    return the JSON response.
+
+    Args:
+        data (dict or OrderedDict): key-value pairs of parameters to post to
+            the API
+
+    Returns:
+        dict
+    """
+    try:
+        kformat = data.pop('format')  # json or xml
+        keyword = data.pop('keyword')
+    except KeyError:
+        url = 'https://bcl.nrel.gov/api/search/'
+    else:
+        url = 'https://bcl.nrel.gov/api/search/{}.{}'.format(keyword, kformat)
+    prepared_url = requests.Request('GET', url, params=data).prepare().url
+    print(prepared_url)
+    cached_response_json = get_from_cache(prepared_url)
+
+    if cached_response_json is not None:
+        # found this request in the cache, just return it instead of making a
+        # new HTTP call
+        return cached_response_json
+
+    else:
+        start_time = time.time()
+        log('Getting from {}, "{}"'.format(url, data))
+        response = requests.get(prepared_url)
+        # if this URL is not already in the cache, pause, then request it
+        # get the response size and the domain, log result
+        size_kb = len(response.content) / 1000.
+        domain = re.findall(r'//(?s)(.*?)/', url)[0]
+        log('Downloaded {:,.1f}KB from {}'
+            ' in {:,.2f} seconds'.format(size_kb, domain,
+                                         time.time() - start_time))
+
+        try:
+            response_json = response.json()
+            if 'remark' in response_json:
+                log('Server remark: "{}"'.format(response_json['remark'],
+                                                 level=lg.WARNING))
+            save_to_cache(prepared_url, response_json)
+        except Exception:
+            # deal with response satus_code here
+            log(
+                'Server at {} returned status code {} and no JSON data.'.format(
+                    domain,
+                    response.status_code),
+                level=lg.ERROR)
+            return response.content
+        else:
+            return response_json
+
+
+def gis_server_raster_request(creds, bbox=None, how='intersects', srid=None,
+                              output_type='Raster'):
+    """
+
+    Args:
+        output_type:
+        creds:
+        bbox:
+        how:
+        srid:
+
+    Returns:
+        numpy.array
+
+    Info:
+        https://gis.stackexchange.com/questions/130139/downloading-raster
+        -data-into-python-from-postgis-using-psycopg2
+    """
+    if gdal is None:
+        raise ImportError('The osgeo package must be installed to use this '
+                          'optional feature. recommended to use conda '
+                          '*install gdal instead* of pip')
+
+    username = creds.pop('username')
+    password = creds.pop('password')
+    server = creds.pop('server')
+    db_name = creds.pop('db_name')
+    tb_schema = creds.pop('schema')
+    table_name = creds.pop('table_name')
+    # create the engine string
+    engine_str = 'postgresql://{}:{}@{}/{}'.format(username, password, server,
+                                                   db_name)
+    # instanciate the server engine
+    engine = create_engine(engine_str)
+
+    xmin, ymin, xmax, ymax = bbox.bounds
+
+    if how.lower() == 'intersects':
+        how = '&&'
+    elif how.lower() == 'contains':
+        how = '@'
+    else:
+        raise NameError('there is no spatial operator named {}. choose from '
+                        '"intersets" or "contains"')
+
+    # prepare the sql query
+    sql = "SELECT ST_AsGDALRaster(ST_Union(rast), 'GTiff') As rast_gdal " \
+          "FROM {schema}.{table_name} " \
+          "WHERE " \
+          "rast " \
+          "{how} " \
+          "ST_MakeEnvelope({xmin}, {ymin}, {xmax}, {ymax}, {my_srid})".format(
+        schema=tb_schema,
+        table_name=table_name,
+        xmin=xmin,
+        ymin=ymin,
+        xmax=xmax,
+        ymax=ymax,
+        my_srid=srid,
+        how=how)
+
+    # try to get results from cache
+    cached_response_geojson = get_from_cache(sql)
+
+    if cached_response_geojson is not None:
+        # found this request in the cache, just return it instead of making a
+        # new sql call. We need to load id usin json.loads though.
+        return gpd.GeoDataFrame.from_features(cached_response_geojson)
+
+    # Use a virtual memory file, which is named like this
+    vsipath = '/vsimem/from_postgis'
+
+    # Download raster data into Python as GeoTIFF, and make a virtual file
+    # for GDAL
+    result = engine.execute(sql)
+
+    try:
+        gdal.FileFromMemBuffer(vsipath, bytes(result.fetchone()[0]))
+
+        # Read first band of raster with GDAL
+        ds = gdal.Open(vsipath)
+        band = ds.GetRasterBand(1)
+        arr = band.ReadAsArray()
+
+    except Exception:
+        # Close and clean up virtual memory file
+        gdal.Unlink(vsipath)
+        raise
+    else:
+        if output_type == 'Raster':
+            gdal.Unlink(vsipath)
+            return ds
+        elif output_type == 'memory':
+            return vsipath
+        elif output_type == 'array':
+            gdal.Unlink(vsipath)
+            return arr
+
+
+def gis_server_request(creds, bbox=None, how='intersects', srid=None):
+    """Send a request to the GIS server via postgis SQL query and return the
+    GeoDataFrame response.
+
+    Args:
+        creds (dict): credentials to connect with the database. Pass a dict
+        containing the 'username', 'password', 'server', 'db_name',
+        'tb_schema', 'engine_str
+        bbox (shapely.geometry): Any shapely geometry that has bounds.
+        how (str): the spatial operator to use. 'intersects' gets more rows
+            while 'contains' gets fewer rows.
+        srid (int): SRID. If no SRID is specified the unknown spatial
+            reference system is assumed.
+
+    Returns:
+        geopandas.GeoDataFrame
+
+    Info:
+        * Originaly from <https://gis.stackexchange.com/questions/83387
+        /performing-bounding-box-query-in-postgis>
+    """
+    username = creds.pop('username')
+    password = creds.pop('password')
+    server = creds.pop('server')
+    db_name = creds.pop('db_name')
+    tb_schema = creds.pop('schema')
+    table_name = creds.pop('table_name')
+    # create the engine string
+    engine_str = 'postgresql://{}:{}@{}/{}'.format(username, password, server,
+                                                   db_name)
+    # instanciate the server engine
+    engine = create_engine(engine_str)
+
+    xmin, ymin, xmax, ymax = bbox.bounds
+
+    if how.lower() == 'intersects':
+        how = '&&'
+    elif how.lower() == 'contains':
+        how = '@'
+    else:
+        raise NameError('there is no spatial operator named {}. choose from '
+                        '"intersets" or "contains"')
+
+    sql = 'SELECT * FROM {schema}.{table_name} ' \
+          'WHERE ' \
+          'geom ' \
+          '{how} ' \
+          'ST_MakeEnvelope({xmin}, {ymin}, {xmax}, {ymax}, {my_srid})'.format(
+        schema=tb_schema,
+        table_name=table_name,
+        xmin=xmin,
+        ymin=ymin,
+        xmax=xmax,
+        ymax=ymax,
+        my_srid=srid,
+        how=how)
+    cached_response_geojson = get_from_cache(sql)
+
+    if cached_response_geojson is not None:
+        # found this request in the cache, just return it instead of making a
+        # new HTTP call. We need to load id usin json.loads though.
+        return gpd.GeoDataFrame.from_features(cached_response_geojson)
+
+    else:
+        start_time = time.time()
+        log('Getting from from {}:{}.{}, "{}"'.format(server, tb_schema,
+                                                      table_name, sql))
+        gdf = gpd.read_postgis(sql, con=engine, geom_col='geom')
+        size_kb = gdf.memory_usage(deep=True).sum() / 1000
+        len_gdf = len(gdf)
+        log('Downloaded {:,.1f} KB or {} entries from {}:{}.{} in '
+            '{:,.2f} seconds'.format(size_kb, len_gdf, server, tb_schema,
+                                     table_name, time.time() - start_time))
+        if not gdf.empty:
+            gdf_json = gdf.to_json()
+            save_to_cache(sql, json.loads(gdf_json))
+        else:
+            log('No entries found. Check your parameters such as the bbox '
+                'coordinates and the CRS', lg.WARNING)
+    return gdf

07cfd600d3fda727a8ec4f5f7587b89809b5e0cf
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2019-01-24 10:24:21 -0500

    Adds Pyomo as a dependency

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     description='',
     install_requires=['numpy', 'pandas', 'eppy', 'sphinx', 'pycountry',
                       'geopandas', 'shapely', 'osmnx', 'pyproj', 'rasterstats',
-                      'scikit-learn'],
+                      'pyomo'],
     test_suite='tests'
 )

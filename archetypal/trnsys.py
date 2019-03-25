8ea6aeeb797b2d48a2f61377ebf607d299c8d0a9
Unknown <louis.leroy5@gmail.com>
2019-03-25 16:46:31 -0400

    add clear_name_idfobjects function

--- archetypal/trnsys.py
+++ archetypal/trnsys.py
@@ -13,6 +13,45 @@ import archetypal as ar
 from archetypal import log
 
 
+def clear_name_idf_objects(idfFile):
+    objs = ['MATERIAL', 'MATERIAL:NOMASS', 'MATERIAL:AIRGAP', 'CONSTRUCTION',
+            'FENESTRATIONSURFACE:DETAILED', 'BUILDINGSURFACE:DETAILED', 'ZONE',
+            'BUILDING', 'SITE:LOCATION']
+    uniqueList = []
+
+    # For all categorie that we want to change Names
+    for obj in objs:
+        epObjects = idfFile.idfobjects[obj]
+
+        # For all objects in Category
+        for epObject in epObjects:
+            # Do not take fenestration, to be treated later
+            fenestration = [s for s in ['fenestration', 'shgc', 'window'] if
+                            s in epObject.Name.lower()]
+            if not fenestration:
+                old_name = epObject.Name
+                # clean old name by removing spaces, "-", period, "{", "}", doubleunderscore
+                new_name = old_name.replace(" ", "_").replace("-", "_").replace(
+                    ".", "_").replace("{", "").replace("}", "").replace("__",
+                                                                        "_")
+                if len(new_name) > 13:
+                    # Trnbuild doen not like names longer than 13 characters
+                    # Replace with unique ID
+                    new_name = uuid.uuid4().hex[:13]
+
+                    # Make sure uuid does not already exist
+                    while new_name in uniqueList:
+                        new_name = uuid.uuid4().hex[:13]
+
+                    uniqueList.append(new_name)
+
+                print("changed layer {} with {}".format(old_name, new_name))
+                modeleditor.rename(idfFile, obj, old_name, new_name)
+
+            else:
+                continue
+
+
 def convert_idf_to_t3d(idf):
     """
 
@@ -24,11 +63,9 @@ def convert_idf_to_t3d(idf):
     """
     start_time = time.time()
     # Load IDF file(s)
-    idfFiles = ar.load_idf(idf)
+    idfFile = ar.load_idf(idf)
     log("IDF files loaded in {:,.2f} seconds".format(time.time() - start_time),
         lg.INFO, name="CoverterLog", filename="CoverterLog")
-
-    print(type(idfFiles[os.path.basename(idf)]))
 
     # Load IDF_T3D template
     start_time = time.time()
@@ -39,4 +76,5 @@ def convert_idf_to_t3d(idf):
     log("IDF_T3D template read in {:,.2f} seconds".format(time.time() - start_time),
         lg.INFO, name="CoverterLog", filename="CoverterLog")
 
-
+    # Clean names of idf objects (e.g. 'MATERIAL')
+    clear_name_idf_objects(idfFile[os.path.basename(idf)])

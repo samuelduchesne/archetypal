36a8fdc7a2975cd41a222ed44be9589fd570ecda
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2019-04-08 19:34:57 -0400

    New: Schedule Class and test files

---
+++ archetypal/schedule.py
@@ -0,0 +1,32 @@
+class Schedule(object):
+    """An object designed to handle any EnergyPlys schedule object"""
+
+
+    @classmethod
+    def from_day_week_year(cls):
+        """
+        Schedule:Day:Hourly
+        Schedule:Day:Interval
+        Schedule:Day:List
+        Schedule:Week:Daily
+        Schedule:Week:Compact
+        Schedule:Year
+
+        Returns:
+
+        """
+        
+
+        pass
+
+    @classmethod
+    def from_compactt(cls):
+        pass
+
+    @classmethod
+    def from_constant(cls):
+        pass
+
+    @classmethod
+    def from_file(cls):
+        pass

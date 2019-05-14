4f58732a65eb60f0644c19de59a145fd8083c0b0
Louis Leroy <louis.leroy@polymtl.ca>
2019-05-14 17:09:39 -0400

    Code cleanup
    - Removes some unnecessary tests
    - Adds more IDF files to trnbuild_from_idf_parallel
    - Provides more useful assert for trnsys tests

--- tests/test_trnsys.py
+++ tests/test_trnsys.py
@@ -15,33 +15,20 @@ def round_up(n, decimals=0):
 
 
 @pytest.mark.win32
-def test_trnbuild_from_idf(config):
-    # Path to IDF file to convert
-
-    window_file = 'W74-lib.dat'
-    window_filepath = os.path.join("tests", "input_data", "trnsys", window_file)
-
-    convert_idf_to_trnbuild(
-        "tests/input_data/trnsys/NECB 2011 - Small Office.idf", window_filepath)
-
-
-@pytest.mark.win32
 def test_trnbuild_from_idf_parallel(config):
     # All IDF files
     idf_list = ["NECB 2011 - Warehouse.idf"]
     # List files here
     file_upper_path = os.path.join('tests', 'input_data', 'trnsys')
-    files = ["NECB 2011 - Warehouse.idf"]
+    files = ["NECB 2011 - Warehouse.idf", "NECB 2011 - Small Office.idf"]
 
     # window_file = 'W74-lib.dat'
     # window_filepath = os.path.join(file_upper_path, window_file)
 
     # prepare args (key=value). Key is a unique id for the runs (here the
     # file basename is used). Value is a dict of the function arguments
-    in_dict = {os.path.basename(file): {'idf_file':
-                                            os.path.join(file_upper_path,
-                                                         file)} for file in
-               files}
+    in_dict = {os.path.basename(file): dict(
+        idf_file=os.path.join(file_upper_path, file)) for file in files}
 
     result = parallel_process(in_dict, convert_idf_to_trnbuild, 4,
                               use_kwargs=True)
@@ -56,7 +43,7 @@ def test_trnbuild_from_idf_parallel_darwin_or_linux(config):
     idf_list = ["NECB 2011 - Warehouse.idf"]
     # List files here
     file_upper_path = os.path.join('tests', 'input_data', 'trnsys')
-    files = ["NECB 2011 - Warehouse.idf"]
+    files = ["NECB 2011 - Warehouse.idf", "NECB 2011 - Small Office.idf"]
 
     window_file = 'W74-lib.dat'
     window_filepath = os.path.join(file_upper_path, window_file)
@@ -70,22 +57,10 @@ def test_trnbuild_from_idf_parallel_darwin_or_linux(config):
         trnidf_exe_dir='docker/trnsidf/trnsidf.exe') for
         file in files}
 
-    parallel_process(in_dict, convert_idf_to_trnbuild, 4, use_kwargs=True)
+    result = parallel_process(in_dict, convert_idf_to_trnbuild, 4,
+                              use_kwargs=True)
 
-
-def test_trnbuild_parse_window_lib(config):
-    file = 'W74-lib.dat'
-    window_filepath = os.path.join("tests", "input_data", "trnsys", file)
-    df, bunches = parse_window_lib(window_filepath)
-
-    assert len(df) == len(bunches)
-
-
-def test_trnbuild_choose_window(config):
-    file = 'W74-lib.dat'
-    window_filepath = os.path.join("tests", "input_data", "trnsys", file)
-    window = choose_window(2.2, 0.64, 0.8, 0.05,
-                           window_filepath)
+    assert not any(isinstance(a, Exception) for a in result.values())
 
 
 @pytest.mark.win32
@@ -93,7 +68,8 @@ def test_trnbuild_idf_win32(config):
     idf_file = "tests/input_data/trnsys/Building.idf"
     template = "tests/input_data/trnsys/NewFileTemplate.d18"
     res = trnbuild_idf(idf_file, template, nonum=True)
-    print(res)
+
+    assert res
 
 
 @pytest.mark.darwin
@@ -106,4 +82,5 @@ def test_trnbuild_idf_darwin_or_linux(config):
                        nonum=False, refarea=False, volume=False,
                        capacitance=True, dck=True)
     # todo: trnidf_exe_dir should point to install path instead of docker path
-    print(res)
+
+    assert res

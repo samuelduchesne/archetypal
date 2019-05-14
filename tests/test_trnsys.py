e4104a8e8fce4be2a493a4a5b42bea6722db961b
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2019-05-14 01:31:22 -0400

    Completes integration of trnsidf.exe
    - Adds pytest.marks for platform specific tests
    - Fixes a possible problem with cd() manager having to switch to a nonexistant path
    - Improved: trnbuild_idf() copies file to settings.data_folder before calling executable + better logs and error handling
    - Overall cosmetic changes and code cleanup

--- tests/test_trnsys.py
+++ tests/test_trnsys.py
@@ -1,6 +1,5 @@
 import math
 import os
-import sys
 
 import pytest
 
@@ -25,25 +24,10 @@ def test_trnbuild_from_idf(config):
         "tests/input_data/trnsys/NECB 2011 - Small Office.idf", window_filepath)
 
 
+@pytest.mark.win32
 def test_trnbuild_from_idf_parallel(config):
     # All IDF files
-    idf_list = ["NECB 2011 - Full Service Restaurant.idf", "NECB 2011 - "
-                                                           "HighRise "
-                                                           "Apartment.idf",
-                "NECB 2011 - Hospital.idf", "NECB 2011 - Large Hotel.idf",
-                "NECB 2011 - Medium Office.idf", "NECB 2011 - MidRise "
-                                                 "Apartment.idf", "NECB 2011 "
-                                                                  "- "
-                                                                  "Outpatient.idf",
-                "NECB 2011 - Primary School.idf",
-                "NECB 2011 - Quick Service Restaurant.idf", "NECB 2011 - "
-                                                            "Retail "
-                                                            "Standalone.idf",
-                "NECB 2011 - Retail Stripmall.idf", "NECB 2011 - "
-                                                    "Secondary School.idf",
-                "NECB 2011 - Small Hotel.idf", "NECB 2011 - Small "
-                                               "Office.idf", "NECB 2011 - "
-                                                             "Warehouse.idf"]
+    idf_list = ["NECB 2011 - Warehouse.idf"]
     # List files here
     file_upper_path = os.path.join('tests', 'input_data', 'trnsys')
     files = ["NECB 2011 - Warehouse.idf"]
@@ -56,6 +40,30 @@ def test_trnbuild_from_idf_parallel(config):
     in_dict = {os.path.basename(file): {'idf_file':
                                             os.path.join(file_upper_path, file),
                                         'window_lib': window_filepath} for
+               file in files}
+
+    parallel_process(in_dict, convert_idf_to_trnbuild, 4, use_kwargs=True)
+
+
+@pytest.mark.darwin
+@pytest.mark.linux
+def test_trnbuild_from_idf_parallel_darwin_or_linux(config):
+    # All IDF files
+    idf_list = ["NECB 2011 - Warehouse.idf"]
+    # List files here
+    file_upper_path = os.path.join('tests', 'input_data', 'trnsys')
+    files = ["NECB 2011 - Warehouse.idf"]
+
+    window_file = 'W74-lib.dat'
+    window_filepath = os.path.join(file_upper_path, window_file)
+
+    # prepare args (key=value). Key is a unique id for the runs (here the
+    # file basename is used). Value is a dict of the function arguments
+    in_dict = {os.path.basename(file): dict(
+        idf_file=os.path.join(file_upper_path, file),
+        window_lib=window_filepath,
+        template="tests/input_data/trnsys/NewFileTemplate.d18",
+        trnidf_exe_dir='docker/trnsidf/trnsidf.exe') for
                file in files}
 
     parallel_process(in_dict, convert_idf_to_trnbuild, 4, use_kwargs=True)
@@ -76,9 +84,22 @@ def test_trnbuild_choose_window(config):
                            window_filepath)
 
 
-@pytest.mark.skipif(sys.platform != "win32", reason="Runs only on Windows")
-def test_trnbuild_idf():
+@pytest.mark.win32
+def test_trnbuild_idf_win32(config):
     idf_file = "tests/input_data/trnsys/Building.idf"
     template = "tests/input_data/trnsys/NewFileTemplate.d18"
     res = trnbuild_idf(idf_file, template, nonum=True)
+    print(res)
+
+
+@pytest.mark.darwin
+@pytest.mark.linux
+def test_trnbuild_idf_darwin_or_linux(config):
+    idf_file = "tests/input_data/trnsys/Building.idf"
+    template = "tests/input_data/trnsys/NewFileTemplate.d18"
+    res = trnbuild_idf(idf_file, template,
+                       trnidf_exe_dir='docker/trnsidf/trnsidf.exe',
+                       nonum=False, refarea=False, volume=False,
+                       capacitance=True, dck=True)
+    # todo: trnidf_exe_dir should point to install path instead of docker path
     print(res)

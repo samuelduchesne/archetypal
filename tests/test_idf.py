958a663fe7b56351a6c9ac365f5cc6211a98b01d
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2019-07-05 00:59:28 -0400

    Makes sure the test is done on a fresh idf

--- tests/test_idf.py
+++ tests/test_idf.py
@@ -64,7 +64,7 @@ def test_load_old(config):
 def test_run_olderv(config, fresh_start, ep_version):
     """Will run eplus on a file that needs to be upgraded with one that does
     not"""
-
+    ar.settings.use_cache=False
     files = ['tests/input_data/problematic/nat_ventilation_SAMPLE0.idf',
              'tests/input_data/regular/5ZoneNightVent1.idf']
     wf = 'tests/input_data/CAN_PQ_Montreal.Intl.AP.716270_CWEC.epw'

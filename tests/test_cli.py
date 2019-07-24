8fa113d1aa61f356cd67d8deccaf91127a5ac283
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2019-07-24 09:48:11 -0400

    Renames the cli reduce test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,11 +6,11 @@ from archetypal.cli import cli
 
 class TestCli():
 
-    def test_hello_world(self, config):
+    def test_reduce(self, config):
         runner = CliRunner()
         examples = get_eplus_dire() / "ExampleFiles"
         test_file = examples / "2ZoneDataCenterHVAC_wEconomizer.idf"
-        test_file = "tests/input_data/umi_samples/B_Off_0.idf"
+        # test_file = "tests/input_data/umi_samples/B_Off_0.idf"
         result = runner.invoke(cli,
                                ['--use-cache', '--cache-folder',
                                 'tests/.temp/cache', '--data-folder',

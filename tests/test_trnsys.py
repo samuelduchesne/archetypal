589fc42e80d73d2dc9813d756501d983cf66e6f1
Unknown <louis.leroy5@gmail.com>
2020-01-30 10:56:53 -0500

    Fix: uses settings.ep_version in tests instead of string

--- tests/test_trnsys.py
+++ tests/test_trnsys.py
@@ -518,7 +518,7 @@ def test_trnbuild_from_idf(config, trnbuild_file):
     # prepare args (key=value). Key is a unique id for the runs (here the
     # file basename is used). Value is a dict of the function arguments
     kwargs_dict = {
-        "ep_version": "8-9-0",
+        "ep_version": settings.ep_version,
         "u_value": 2.5,
         "shgc": 0.6,
         "t_vis": 0.78,

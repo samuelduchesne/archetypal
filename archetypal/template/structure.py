cce0dfba800bf6a1cc41d97b55ff9f951b807c6b
Unknown <louis.leroy5@gmail.com>
2020-04-28 11:31:23 -0400

    Deprecation of from_json() for StructureDefinition class

--- archetypal/template/structure.py
+++ archetypal/template/structure.py
@@ -6,6 +6,8 @@
 ################################################################################
 
 import collections
+import deprecation
+import archetypal
 
 from archetypal.template import UmiBase, Unique, OpaqueMaterial, UniqueName
 
@@ -109,6 +111,9 @@ class StructureDefinition(UmiBase, metaclass=Unique):
             )
 
     @classmethod
+    @deprecation.deprecated(deprecated_in="1.3.1", removed_in="1.4",
+                            current_version=archetypal.__version__,
+                            details="Use from_dict function instead")
     def from_json(cls, *args, **kwargs):
         """
         Args:

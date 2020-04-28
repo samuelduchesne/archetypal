a9c6c9dd10a2a58ea8e3fcd98f06186907ea0a08
Unknown <louis.leroy5@gmail.com>
2020-04-28 11:37:57 -0400

    Deprecation of from_json() for WindowSetting class

--- archetypal/template/window.py
+++ archetypal/template/window.py
@@ -750,6 +750,9 @@ class WindowSetting(UmiBase, metaclass=Unique):
         return data_dict
 
     @classmethod
+    @deprecation.deprecated(deprecated_in="1.3.1", removed_in="1.4",
+                            current_version=archetypal.__version__,
+                            details="Use from_dict function instead")
     def from_json(cls, *args, **kwargs):
         """
         Args:

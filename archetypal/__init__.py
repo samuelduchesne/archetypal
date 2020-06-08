9ea590fdb9196cbcd2f8be17420eb4435cf72869
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2020-06-08 12:40:41 -0400

    Bumped to version 1.3.2

--- archetypal/__init__.py
+++ archetypal/__init__.py
@@ -6,7 +6,7 @@
 ################################################################################
 
 # Version of the package
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 # warn if a newer version of archetypal is available
 from outdated import warn_if_outdated

5962dbd8f0096c873de14880d005a88185883831
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-03-16 13:48:37 -0400

    updates 2 measures with tests

--- archetypal/template/measure.py
+++ archetypal/template/measure.py
@@ -1,12 +1,21 @@
 """Energy measures modules."""
-from archetypal.template import ZoneLoad, BuildingTemplate
 import logging
+
+from archetypal.template import BuildingTemplate, MaterialLayer
 
 log = logging.getLogger(__name__)
 
 
 class Measure:
-    """Main class for the definition of EnergyMeasures."""
+    """Main class for the definition of measures.
+
+    Args:
+        name (str): The name of the measure.
+        description (str): A description of the measure.
+    """
+
+    name = ""
+    description = ""
 
     def __init__(self):
         pass
@@ -31,13 +40,49 @@ class Measure:
         for a in dir(Measure):
             yield a
 
+    def __repr__(self):
+        return self.description
+
 
 class EnergyStarUpgrade(Measure):
     """The EnergyStarUpgrade changes the equipment power density to."""
 
-    def __init__(self):
-        super().__init__()
+    name = "EnergyStarUpgrade"
+    description = "EnergyStar for tenant spaces of 0.75 W/sf ~= 8.07 W/m2"
 
-        self.SetEquipementPowerDensity = lambda bt: setattr(
-            bt.Core.Loads, "EquipmentPowerDensity", 3
+    def __init__(self):
+        super(EnergyStarUpgrade, self).__init__()
+
+        self.SetCoreEquipementPowerDensity = lambda building_template: setattr(
+            building_template.Core.Loads, "LightingPowerDensity", 8.07
         )
+        self.SetPerimEquipementPowerDensity = lambda building_template: setattr(
+            building_template.Perimeter.Loads, "LightingPowerDensity", 8.07
+        )
+
+
+class SetFacadeConstructionThermalResistanceToEnergyStar(Measure):
+    """This measure changes the r-value of the insulation layer of the facade
+    construction to R5.78.
+    """
+
+    name = "SetFacadeConstructionThermalResistanceToEnergyStar"
+    description = (
+        "This measure changes the r-value of the insulation layer of the "
+        "facade construction to R5.78."
+    )
+
+    def __init__(self):
+        super(SetFacadeConstructionThermalResistanceToEnergyStar, self).__init__()
+
+        self.AddThermalInsulation = self.apply
+
+    def apply(self, bt):
+        """Only apply to Perimeter facade constructions."""
+        self.set_insulation_layer_resistance(bt.Perimeter.Constructions.Facade)
+
+    @staticmethod
+    def set_insulation_layer_resistance(oc):
+        """Set the insulation later to r_value = 1.02."""
+        layer: MaterialLayer = oc.infer_insulation_layer()
+        layer.r_value = 1.017858  # R5.78 IP, will change the conductivity of the mat.

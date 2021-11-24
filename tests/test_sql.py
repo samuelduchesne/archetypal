02c85dfb038ed54667267c1991336daacb24d125
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-11-24 11:08:22 -0500

    Refactored as a class

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -3,7 +3,7 @@ import itertools
 import pytest
 
 from archetypal import IDF
-from archetypal.idfclass.sql import collect_output_by_name, available_datapoints
+from archetypal.idfclass.sql import Sql
 
 metered_ressource_types = (
     "Electricity",
@@ -33,14 +33,14 @@ all_meters = list(itertools.product(end_use_category_types, metered_ressource_ty
 all_meter_names = [f"{a}:{b}" for a, b in all_meters]
 
 
-@pytest.fixture()
-def sql_file():
+@pytest.fixture(scope="session")
+def sql():
     idf = IDF.from_example_files(
         "RefBldgMediumOfficeNew2004_Chicago.idf",
         "USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw",
     ).simulate()
 
-    yield idf.sql_file
+    yield Sql(idf.sql_file)
 
 
 @pytest.mark.parametrize(
@@ -48,29 +48,45 @@ def sql_file():
     (all_meter_names[0], [all_meter_names[0]], all_meter_names),
     ids=("str", "one", "multiple"),
 )
-def test_collect_meters(variable_or_meter, sql_file):
+def test_collect_meters(variable_or_meter, sql):
     """Test collecting outputs by name."""
 
-    df = collect_output_by_name(
-        sql_file, variable_or_meter, reporting_frequency="Hourly"
-    )
+    df = sql.collect_output_by_name(variable_or_meter, reporting_frequency="Hourly")
 
     assert not df.empty
 
 
-def test_available_datapoints(sql_file):
+def test_available_datapoints(sql):
     """Test getting available datapoints in the sql_file"""
-    dps = available_datapoints(sql_file)
+    dps = sql.available_outputs
     print(dps)
 
     assert dps
 
 
-def test_collect_variables(sql_file):
+def test_collect_variables(sql):
     variable_or_meter = ("Zone Mean Air Temperature",)
 
-    df = collect_output_by_name(
-        sql_file, variable_or_meter, reporting_frequency="Hourly"
-    )
+    df = sql.collect_output_by_name(variable_or_meter, reporting_frequency="Hourly")
 
     assert not df.empty
+
+
+def test_collect_variables_raise(sql):
+    """Raise error if reporting frequency not supported."""
+    variable_or_meter = ("Zone Mean Air Temperature",)
+
+    with pytest.raises(AssertionError):
+        sql.collect_output_by_name(variable_or_meter, reporting_frequency="all")
+
+
+def test_zone_info(sql):
+    df = sql.zone_info
+
+    print(df)
+
+
+def test_environment_periods(sql):
+    df = sql.environment_periods
+
+    print(df)

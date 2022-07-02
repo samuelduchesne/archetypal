9f9bdd0a0347de987682bf1040ba1b3152e15646
szvsw <mail@samwolk.info>
2022-07-02 11:56:18 -0400

    merge measures from feature/bundle

---
+++ tests/test_measure.py
@@ -0,0 +1,103 @@
+"""Test measures module."""
+import pytest
+
+from archetypal.template.measures.measure import (
+    EnergyStarUpgrade,
+    FacadeUpgradeBest,
+    FacadeUpgradeLow,
+    FacadeUpgradeMid,
+    FacadeUpgradeRegular,
+    SetFacadeConstructionThermalResistanceToEnergyStar,
+    InfiltrationRegular,
+    InfiltrationTight,
+    InfiltrationMedium,
+)
+
+
+@pytest.fixture(scope="function")
+def umi_library():
+    """Yield an umi library for tests."""
+    from archetypal import UmiTemplateLibrary
+
+    umi = UmiTemplateLibrary.open(
+        "tests/input_data/umi_samples/BostonTemplateLibrary_nodup.json"
+    )
+    yield umi
+
+
+class TestMeasure:
+    """Test Measures on UmiTemplateLibraries."""
+
+    def test_apply_measure_to_single_building_template(self, umi_library):
+        """Test applying measure only to a specific building template."""
+        building_template = umi_library.BuildingTemplates[0]
+
+        assert umi_library.BuildingTemplates[0].Core.Loads.LightingPowerDensity == 12.0
+
+        EnergyStarUpgrade().apply_measure_to_template(building_template)
+
+        assert umi_library.BuildingTemplates[0].Core.Loads.LightingPowerDensity == 8.07
+        assert umi_library.BuildingTemplates[1].Core.Loads.LightingPowerDensity == 16.0
+
+    def test_apply_measure_to_whole_library(self, umi_library):
+        """Test applying measure to whole template library."""
+        assert umi_library.BuildingTemplates[0].Core.Loads.LightingPowerDensity == 12.0
+
+        # apply the measure
+        EnergyStarUpgrade().apply_measure_to_whole_library(umi_library)
+
+        # Assert the value has changed for all ZoneLoads objects.
+        for zone_loads in umi_library.ZoneLoads:
+            assert zone_loads.LightingPowerDensity == 8.07
+
+        oc = umi_library.BuildingTemplates[3].Perimeter.Constructions.Facade
+        previous_thickness = oc.total_thickness
+        previous_r_value = oc.r_value
+
+        measure = SetFacadeConstructionThermalResistanceToEnergyStar(
+            rsi_value_facade=3.08, rsi_value_roof=7.2
+        )
+        measure.apply_measure_to_whole_library(umi_library)
+
+        # assert that the total wall r_value has increased.
+        assert oc.r_value > previous_r_value
+
+        # assert that the total wall thickness has increased since setting the
+        # r-value increases the thickness of the material.
+        assert oc.total_thickness > previous_thickness
+
+    @pytest.mark.parametrize(
+        "measure",
+        [
+            FacadeUpgradeBest(),
+            FacadeUpgradeMid(),
+            FacadeUpgradeRegular(),
+            FacadeUpgradeLow(),
+        ],
+    )
+    def test_facade_upgrade(self, measure, umi_library):
+        """Test series of facade upgrades using parametrization."""
+        # apply the measure
+        facade = umi_library.BuildingTemplates[3].Perimeter.Constructions.Facade
+        i = facade.infer_insulation_layer()
+        oc = facade.Layers[i]
+
+        measure.apply_measure_to_whole_library(umi_library)
+
+        # assert that the total wall r_value has increased.
+        assert oc.r_value == pytest.approx(measure.rsi_value_facade)
+
+    @pytest.mark.parametrize(
+        "measure, infiltration_ach",
+        [
+            (InfiltrationRegular(), 0.6),
+            (InfiltrationMedium(), 0.3),
+            (InfiltrationTight(), 0.1),
+        ],
+    )
+    def test_infiltration_upgrade(self, measure, infiltration_ach, umi_library):
+        """Test applying the infiltration measures."""
+        measure.apply_measure_to_whole_library(umi_library)
+
+        for bldg in umi_library.BuildingTemplates:
+            assert bldg.Perimeter.Ventilation.Infiltration == infiltration_ach

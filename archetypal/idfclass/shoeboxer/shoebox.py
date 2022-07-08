53a87915f9b1ea6b1a36f8d277b43367a0b48330
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2022-07-08 11:13:01 -0400

    cleans logger in module

--- archetypal/idfclass/shoeboxer/shoebox.py
+++ archetypal/idfclass/shoeboxer/shoebox.py
@@ -20,21 +20,7 @@ from validator_collection import checkers, validators
 
 from .hvac_templates import HVACTemplates
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
+log = logging.getLogger(__name__)
 
 
 class ShoeBox(IDF):

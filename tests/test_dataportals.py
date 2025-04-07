1a1c8ccd251b7386f3cdeb644e207038f34a24a0
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2025-04-07 10:54:14 -0400

    Deprecates StatCan module and skips tests. (#531)
    Adds a deprecation warning to the StatCan module to indicate its obsolescence.
    
    Marks StatCan tests as skipped due to deprecated APIs, preventing failures
    during automated testing.

--- tests/test_dataportals.py
+++ tests/test_dataportals.py
@@ -219,6 +219,7 @@ def test_download_and_load_bld_window(config):
     assert ws
 
 
+@pytest.mark.skip("APIs seem deprecated.")
 def test_statcan(config):
     data = {"response_format": "json", "lang": "E", "dguid": "2016A000011124", "topic": 5, "notes": 0}
     response = dataportal.stat_can_request(**data)
@@ -228,6 +229,7 @@ def test_statcan(config):
     assert response
 
 
+@pytest.mark.skip("APIs seem deprecated.")
 def test_statcan_error(config):
     # Tests statcan with error in inputs
     data = {"response_format": "json", "lang": "E", "dguid": "wrong_string", "topic": 5, "notes": 0}
@@ -238,6 +240,7 @@ def test_statcan_error(config):
     assert response is None
 
 
+@pytest.mark.skip("APIs seem deprecated.")
 def test_statcan_geo(config):
     data = {"response_format": "json", "lang": "E", "geos": "PR", "cpt": "00"}
     response = dataportal.stat_can_geo_request(**data)
@@ -247,6 +250,7 @@ def test_statcan_geo(config):
     assert response
 
 
+@pytest.mark.skip("APIs seem deprecated.")
 def test_statcan_geo_error(config):
     # Tests statcan_geo with error in inputs
     data = {"response_format": "json", "lang": "E", "geos": "wrong_string", "cpt": "00"}
